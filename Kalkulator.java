c7b361fb79c9ec507acb69a2d7584371e5665e03
Gabriel Panji Lazuardi <32382701+Bephometh@users.noreply.github.com>
2018-11-02 14:33:23 +0700

    Update Kalkulator.java

--- Kalkulator.java
+++ Kalkulator.java
@@ -23,14 +23,15 @@ public class Kalkulator
     
     public float multiply(float a,float b){
         
-		
+	return a*b;	
     }
     
     public float division(float a,float b){
         
-		
+	return a/b;
     }
 	
+
 	public float max (float a, float b){
 		if( a < b){
 			return b;
@@ -39,4 +40,10 @@ public class Kalkulator
 			return a;
 		}	
 	}
+
+    public float min(float a,float b){
+	if(a<b)return a;
+	else return b;
+    }
+=======
 }
