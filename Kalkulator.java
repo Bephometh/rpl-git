9642ef92b110eda1eccc89e769a0ac979d0c770e
Anugrah Jaya <30459937+AnugrahJaya1@users.noreply.github.com>
2018-11-02 13:24:56 +0700

    Initial commit

---
+++ Kalkulator.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class Kalkulator here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Kalkulator
+{
+    public Kalkulator(){
+        
+    }
+    
+    public float plus(float a,float b){
+        return a+b;
+    }
+    
+    public float minus(float a,float b){
+        return a-b;
+    }
+    
+    public float multiply(float a,float b){
+        return a*b;
+    }
+    
+    public float division(float a,float b){
+        return a/b;
+    }
+}
