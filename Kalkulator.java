951b16a8aa8362220b277f1beadb9a5f59ad5a40
Anugrah Jaya <30459937+AnugrahJaya1@users.noreply.github.com>
2018-11-02 13:27:51 +0700

    Initial commit

--- Kalkulator.java
+++ Kalkulator.java
@@ -12,18 +12,22 @@ public class Kalkulator
     }
     
     public float plus(float a,float b){
-        return a+b;
+        
+		
     }
     
     public float minus(float a,float b){
-        return a-b;
+        
+		
     }
     
     public float multiply(float a,float b){
-        return a*b;
+        
+		
     }
     
     public float division(float a,float b){
-        return a/b;
+        
+		
     }
 }
